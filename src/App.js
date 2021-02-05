333ec5fd8d6b13b17eff78481ce62877e3e5f2f8
HYUNGTAE KIM <rlagudxo9774@naver.com>
2021-02-05 17:40:39 +0900

    04-3 map  인자 이용하여 사진 넣기

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React from 'react';
+import { findRenderedDOMComponentWithClass } from 'react-dom/test-utils';
 
-function Food({fav}) {
-  return <h1>I like {fav}</h1>
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>);
+      <img src={picture} />
+
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    name: 'Kimchi',
+    image: 'https://wkiss.wikim.re.kr/upload/board/kind/150223102837_2886734975_FRjlQ7he.jpg'
+  },
+  {
+    name: 'Samgyeopsal',
+    image: 'https://image.fmkorea.com/files/attach/new/20200213/486616/959741723/2703711375/21fa12dc9af4fe710ce3d463affa3b96.jpg'
+  },
+  {
+    name: 'Bibimbap',
+    image: 'http://www.koreahousedallas.com/files/attach/images/20400/439/020/d1fea530fcaad8974440ad5325631c3b.JPG'
+  }
+];
 
 function App() {
   //return <div className="App" />;
   return (
   <div>
-    <h1>FUCK YOU</h1>
-    <Food fav="kimchi" />
-    <Food fav="ramen" />
-    <Food fav="samgiopsal" />
-    <Food fav="chukumi" />
+    {foodILike.map(dish => (
+    <Food name={dish.name} picture={dish.image} />
+    ))}
   </div>);
 }
 
